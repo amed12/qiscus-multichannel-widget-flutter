679c53d645ebfddc13d255ad1647953f1bb680fb
Achmad Fathullah <fathullah@qiscus.co>
2025-07-29 22:28:23 +0700

    feat: migrate Android build system to Gradle Kotlin DSL and update dependencies

---
+++ example/android/app/src/main/kotlin/com/example/multichannel_flutter_sample/multichannel_flutter_sample/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.multichannel_flutter_sample.multichannel_flutter_sample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ example/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/achmadfathullah/.asdf/installs/flutter/3.32.8-stable"
+export "FLUTTER_APPLICATION_PATH=/Users/achmadfathullah/Documents/flutter/oc-flutter/example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.2.2"
+export "FLUTTER_BUILD_NUMBER=1.2.2"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
